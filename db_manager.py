087202583d9b14cb5ed5a5cea839ef1d2f376506
bonjih <bonjih@gmail.com>
2021-08-20 17:27:03 +1000

    cleaned script

--- db_manager.py
+++ db_manager.py
@@ -34,6 +34,7 @@ def check_entry_exist(entry, exits):
         return False
 
 
+# need to change to a trigger
 def check_id_exist(urlID, url_id):
     i = [list(i) for i in urlID]
     res = any(url_id in sublist for sublist in i)
@@ -209,11 +210,12 @@ def image_blob_to_db(mag_names, s_type):
     for i, j in zip(img_url_id, img_url):
         img_url_id = i[0]
         img_url = j[0]
-        print(img_url_id, s_type)
+        #print(img_url_id, s_type)
         image_page = requests.get(img_url)
         if image_page.status_code == 200:
             img_bin = url_to_image(img_url)
 
+            # a dumb way of adding eh img_url_ids, should use trigger
             # occasionally index error here
             cursor.execute("SELECT img_url_id FROM images")
             exits = cursor.fetchall()
@@ -224,7 +226,8 @@ def image_blob_to_db(mag_names, s_type):
                     "INSERT INTO images (img_url_id, mag_name_id, site_type, image)" "VALUES(%s, %s, %s, %s)",
                     (img_url_id, m_name_id, s_type, img_bin))
             else:
-                print('skipping, img_url_id existing', img_url_id)
+                pass
+                #print('skipping, img_url_id existing', img_url_id)
 
         conn.commit()
 
